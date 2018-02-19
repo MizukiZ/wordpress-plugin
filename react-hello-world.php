41c73ffa1d2ce8ccb2c58373022e0d101f7cc0c8
Mizuki Zenta <mizukisb@gmail.com>
2018-02-20 00:01:40 +1100

    add actionhero file and use injection func

--- react-hello-world.php
+++ react-hello-world.php
@@ -12,13 +12,23 @@ function wp_react_hello_world() {
 }
  
 function include_react_files() {
-    wp_enqueue_style( 'prefix-style', plugins_url('css\main.d33e78b3.css', __FILE__) );
-    wp_enqueue_script( 'plugin-scripts', plugins_url('js/main.56d42d4b.js', __FILE__),array(),  '0.0.1', true );
+    wp_enqueue_style( 'prefix-style', plugins_url('css/main.c17080f1.css', __FILE__) );
+    wp_enqueue_script( 'plugin-scripts', plugins_url('js/main.c99d7070.js', __FILE__),array(),  '0.0.1', true );
 
-    wp_localize_script('plugin-scripts','wp_home_url', array(
-      "base_url" => home_url()
+    wp_localize_script('plugin-scripts','cm_device_info', array(
+      "api_key" => get_option("api_key"),
+      "app_id" => get_option("app_id"),
+      "device_id" => get_option("device_id")
     ));
 }
+
+// add actionheroclient in head
+function actionhero_js() {
+  $url = plugins_url('actionheroClient.js', __FILE__);
+  echo '<script type="text/javascript" src= '.$url.' ></script>';
+}
+
+add_action('wp_head', 'actionhero_js');
 
 /**
    * Register a book post type, with REST API support
